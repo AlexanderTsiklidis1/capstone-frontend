00b1661b4c15fe1dfa101afef26b8cb6e7802052
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-02-25 10:37:49 -0500

    imports firebase and sets up firebase console

---
+++ src/Services/Firebase.js
@@ -0,0 +1,50 @@
+import { getApp, initializeApp, deleteApp,  } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { GoogleAuthProvider, signInWithPopup, getAuth, signOut } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
+};
+
+export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+
+auth.useDeviceLanguage();
+
+
+const googleAuth = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+    try {
+  //the signInWithPopUp() method accepts ANY provider we create. This is all our authentication logic
+    signInWithPopup(auth, googleAuth).then((res) => {
+        console.log(res)
+    });
+     } catch (err) {
+      console.log("test3")
+      console.log(err, "hello2");
+      console.log("test4")
+     }
+  };
+
+  export const logOut = async () =>{
+    try {
+      await signOut(auth)
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+
+const analytics = getAnalytics(app);
