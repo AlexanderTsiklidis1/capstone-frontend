af134aba94b23455a3810686532ef94562aca429
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-03-03 20:47:39 -0500

    adds prelim navbar to app

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './Navbar.css'; // Make sure you have a corresponding CSS file for styling
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="menu-icon" onClick={toggleDropdown}>
+        {/* Hamburger Icon */}
+        <div className="hamburger-icon">
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+        </div>
+
+        {isOpen && (
+          <div className="dropdown-menu">
+            <a href="/home" onClick={toggleDropdown}>Home</a>
+            <a href="/dashboard" onClick={toggleDropdown}>Dashboard</a>
+            <a href="/book-interview" onClick={toggleDropdown}>Book an Interview</a>
+            <a href="/interview-feedback" onClick={toggleDropdown}>Interview Feedback</a>
+            <a href="/interview-prompts" onClick={toggleDropdown}>Interview Prompts</a>
+          </div>
+        )}
+      </div>
+
+      <div className="logo">Acelt</div>
+
+      <div className="login-signup">
+        <a href="/login-signup" className="login-signup-button">LOGIN / SIGN UP</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
