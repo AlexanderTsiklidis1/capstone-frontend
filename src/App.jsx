b5cc499cbd4c489f2bc96ab073df47380f027fdc
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-03-05 18:33:57 -0500

    adds some styling to nav/landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import LandingPage from './Components/LandingPage';
+import LandingPage from './Pages/LandingPage';
 import MeetingPage from './Pages/MeetingPage';
 
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
   return (
     <div className="App">
       <header className="App-header"></header>
