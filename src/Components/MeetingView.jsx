f8f9a11d3c9bf8167e8ea4d2f1b0455c18801ed2
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-03-05 17:39:46 -0500

    breaks up code on app.jsx into components and singular page, parses imports to correct files as well

---
+++ src/Components/MeetingView.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { useMeeting } from '@videosdk.live/react-sdk';
+import ParticipantView from './ParticipantView';
+import Controls from './Controls';
+
+function MeetingView({ meetingId, onMeetingLeave }) {
+  const [joined, setJoined] = useState(null);
+  const { join, participants } = useMeeting({
+    onMeetingJoined: () => setJoined("JOINED"),
+    onMeetingLeft: () => onMeetingLeave(),
+  });
+
+  const joinMeeting = () => {
+    setJoined("JOINING");
+    join();
+  };
+
+  return (
+    <div className="container">
+      <h3>Meeting Id: {meetingId}</h3>
+      {joined === "JOINED" ? (
+        <div>
+          <Controls />
+          {[...participants.keys()].map((participantId) => (
+            <ParticipantView participantId={participantId} key={participantId} />
+          ))}
+        </div>
+      ) : joined === "JOINING" ? (
+        <p>Joining the meeting...</p>
+      ) : (
+        <button onClick={joinMeeting}>Join</button>
+      )}
+    </div>
+  );
+}
+
+export default MeetingView;
---
+++ src/Pages/MeetingPage.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import JoinScreen from '../Components/JoinScreen';
+import MeetingView from '../Components/MeetingView';
+import {MeetingProvider} from "@videosdk.live/react-sdk";
+import { authToken, createMeeting } from '../API'; // Ensure this path matches your project structure
+
+function MeetingPage() {
+  const [meetingId, setMeetingId] = useState(null);
+
+  const getMeetingAndToken = async (id) => {
+    const newMeetingId = id == null ? await createMeeting({ token: authToken }) : id;
+    setMeetingId(newMeetingId);
+  };
+
+  const onMeetingLeave = () => {
+    setMeetingId(null);
+  };
+
+  return (
+    <>
+
+      {authToken && meetingId ? (
+    <MeetingProvider
+        config={{
+          meetingId,
+          micEnabled: true,
+          webcamEnabled: true,
+          name: "C.V. Raman",
+        }}
+        token={authToken}
+        >
+          <MeetingView meetingId={meetingId} onMeetingLeave={onMeetingLeave} />
+          </MeetingProvider>
+          ) : (
+              <JoinScreen getMeetingAndToken={getMeetingAndToken} />
+              )}
+    </>
+  );
+}
+
+export default MeetingPage;
