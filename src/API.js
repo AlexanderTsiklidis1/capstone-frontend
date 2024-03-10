63ce685ca5e281ffe9b16cf1320eb1a228923d6f
Anthony Huarneck <anthonyhuarneck@Anthonys-MacBook-Air.local>
2024-03-10 15:35:26 -0400

    API.js file edited in order to auto-generate authToken.

--- src/API.js
+++ src/API.js
@@ -1,27 +1,35 @@
-//This is the Auth token, you will use it to generate a meeting and connect to it
+let authToken = "";
 
-
-/* 
-
- *** ABSOLUTELY make sure new server is spun and brand new token is fetched from URL/get-token EVERY 1000minutes!!! Token below will not work as it was fetched days ago & possibly from a new server instance!!!***
-
-*/
-
-//This is the Auth token, you will use it to generate a meeting and connect to it
-
-export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiJhYjk2YzRkZi1kOTFjLTRjODgtYjE0Zi1iZmU1MDk0M2JmNDIiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIiwiYWxsb3dfbW9kIl0sImlhdCI6MTcxMDA4NDY0MCwiZXhwIjoxNzEwMTQ0NjQwfQ.C1BPfscmJ7NKY1HkyapZNBIAOs9wrPkM5TgpMyOamS0";
-
-// API call to create a meeting
-export const createMeeting = async ({ token }) => {
-  const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
-    method: "POST",
-    headers: {
-      authorization: `${authToken}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
-  });
-  //Destructuring the roomId from the response
-  const { roomId } = await res.json();
-  return roomId;
+// API call to fetch authToken from backend
+export const fetchAuthToken = async () => {
+  try {
+    const res = await fetch("http://localhost:9000/get-token");
+    const data = await res.json();
+    authToken = data.authToken;
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+// API call to create a meeting using the fetched authToken
+export const createMeeting = async () => {
+  try {
+    if (!authToken) {
+      await fetchAuthToken();
+    }
+    const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
+      method: "POST",
+      headers: {
+        authorization: `${authToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    });
+    const { roomId } = await res.json();
+    return roomId;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { authToken };
