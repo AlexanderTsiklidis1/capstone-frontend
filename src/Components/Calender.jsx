253d606f51eb9668c6584998e6ff5bef75e18dca
Alexander Tsiklidis <alex@Alexanders-MacBook-Air.local>
2024-03-17 14:11:11 -0400

    builds out more functionality in fullValendar import

--- src/Components/Calender.jsx
+++ src/Components/Calender.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
-import FullCalendar from '@fullcalendar/react'; // Main component
-import dayGridPlugin from '@fullcalendar/daygrid'; // Plugin for day grid view
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid'; // For day grid view
+import timeGridPlugin from '@fullcalendar/timegrid'; // For time grid views
+import interactionPlugin from "@fullcalendar/interaction"; // For clickable dates and events
+import listPlugin from '@fullcalendar/list'; // For list view
 
-const Calendar = () => {
+export default function Calendar() {
+  
+  const handleDateClick = (arg) => {
+    alert('Date clicked: ' + arg.dateStr);
+  };
+
+  
+  const handleEventClick = (clickInfo) => {
+    alert('Event clicked: ' + clickInfo.event.title);
+  };
+
+  
+  const addEvent = (event) => {
+    
+  };
+
   return (
-    <div>
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
-        weekends={true}
-        events={[
-          { title: 'Event 1', date: '2024-03-17' },
-          // Add more events here
-        ]}
-      />
-    </div>
+    <FullCalendar
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+      headerToolbar={{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      }}
+      initialView='dayGridMonth'
+      editable={true}
+      selectable={true}
+      selectMirror={true}
+      dayMaxEvents={true}
+      weekends={true}
+      select={handleDateClick} 
+      events={[
+       
+        { title: 'Event 1', start: new Date().toISOString(), end: new Date().toISOString() }
+        
+      ]}
+      eventClick={handleEventClick}
+    />
   );
-};
-
-export default Calendar;
+}
