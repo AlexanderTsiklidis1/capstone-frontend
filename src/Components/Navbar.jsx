d7cc1463f8188c2af3330162933c72bd23f3c37d
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-03-23 16:36:03 -0400

    Update 'NavBar'to be consistent throughout app.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,72 +1,98 @@
 import React, { useState, useContext, useEffect } from 'react';
-import './Navbar.css'; 
-import './Button.css'
-import { signInWithGoogle, logOut } from "../Services/Firebase.js";
-import { UserContext } from "../Providers/UserProvider";
-import { useNavigate } from "react-router-dom";
+import './Navbar.css';
+import './Button.css';
+import { signInWithGoogle, logOut } from '../Services/Firebase.js';
+import { UserContext } from '../Providers/UserProvider';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  
-  const navigate = useNavigate();
+	const [isOpen, setIsOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
+	const navigate = useNavigate();
 
-  const handleLogout = () => {
-    logOut();
-    navigate("/");
-  }
+	const toggleDropdown = () => {
+		setIsOpen(!isOpen);
+	};
 
-  const user = useContext(UserContext);
-    console.log(user)
+	const handleLogout = () => {
+		logOut();
+		navigate('/');
+	};
 
+	const user = useContext(UserContext);
+	console.log(user);
 
+	return (
+		<nav className='navbar'>
+			{/* {user ? (
+				<>
+					<a href='/userDashboard' className='button'>
+						Dashboard
+					</a>
+					<a href='/book-interview' className='button'>
+						Book an Interview
+					</a>
+					<div className='logo'>AceIT</div>
+					<a href='/resources' className='button'>
+						Resources
+					</a>
+					<a href='/prompts' className='button'>
+						Interview Prompts
+					</a>
+					<a href='/calendar2' className='button' onClick={toggleDropdown}>
+						Calendar
+					</a>
+					<button onClick={handleLogout} className='button'>
+						LOGOUT
+					</button>
+				</>
+			) : ( */}
+			<>
+				<div className='menu-icon' onClick={toggleDropdown}>
+					<div className='hamburger-icon'>
+						<div className='line' />
+						<div className='line' />
+						<div className='line' />
+					</div>
 
+					{isOpen && (
+						<div className='dropdown-menu'>
+							<a href='/' onClick={toggleDropdown}>
+								Home
+							</a>
+							<a href='/userDashboard' onClick={toggleDropdown}>
+								Dashboard
+							</a>
+							<a href='/book-interview' onClick={toggleDropdown}>
+								Book an Interview
+							</a>
+							<a href='/resources' onClick={toggleDropdown}>
+								Resources
+							</a>
+							<a href='/prompts' onClick={toggleDropdown}>
+								Interview Prompts
+							</a>
+							<a href='/calendar2' onClick={toggleDropdown}>
+								Calendar
+							</a>
+						</div>
+					)}
+				</div>
 
-  return (
-    <nav className="navbar">
-      {user ? (
-        <>
-         <a href="/userDashboard" className="button">Dashboard</a>
-          <a href="/book-interview" className="button">Book an Interview</a>
-          <div className="logo">AceIT</div>
-          <a href="/resources" className="button">Resources</a>
-          <a href="/prompts" className="button">Interview Prompts</a>
-          <a href="/calendar2" className='button' onClick={toggleDropdown}>Calendar</a>
-          <button onClick={handleLogout} className="button">LOGOUT</button>
-        </>
-      ) : (
-        <>
-          <div className="menu-icon" onClick={toggleDropdown}>
-            <div className="hamburger-icon">
-              <div className="line" />
-              <div className="line" />
-              <div className="line" />
-            </div>
+				<div className='logo'>AceIT</div>
 
-            {isOpen && (
-              <div className="dropdown-menu">
-                <a href="/" onClick={toggleDropdown}>Home</a>
-                <a href="/userDashboard" onClick={toggleDropdown}>Dashboard</a>
-                <a href="/book-interview" onClick={toggleDropdown}>Book an Interview</a>
-                <a href="/resources" onClick={toggleDropdown}>Resources</a>
-                <a href="/prompts" onClick={toggleDropdown}>Interview Prompts</a>
-                <a href="/calendar2" onClick={toggleDropdown}>Calendar</a>
-              </div>
-            )}
-          </div>
-
-          <div className="logo">AceIT</div>
-
-          <div className="login-signup">
-            <button onClick={signInWithGoogle} className="button login-signup-button">LOGIN / SIGN UP</button>
-          </div>
-        </>
-      )}
-    </nav>
-  );
+				<div className='login-signup'>
+					<button
+						onClick={user ? handleLogout : signInWithGoogle}
+						className='button login-signup-button'
+					>
+						{user ? `LOGOUT` : `LOGIN / SIGN UP`}
+					</button>
+				</div>
+			</>
+			{/* )} */}
+		</nav>
+	);
 };
 
 export default Navbar;
