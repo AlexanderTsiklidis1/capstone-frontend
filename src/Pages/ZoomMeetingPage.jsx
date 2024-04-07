411a73f50e145936eaa56c0dda1f9cc4550990a9
Anthony Huarneck <anthonyhuarneck@Anthonys-MacBook-Air.local>
2024-04-07 16:40:15 -0400

    updates zoomMeetingPage.jsx

--- src/Pages/ZoomMeetingPage.jsx
+++ src/Pages/ZoomMeetingPage.jsx
@@ -1,40 +1,48 @@
-import React from 'react';
-import { ZoomMtg } from '@zoom/meetingsdk';
-import { UserContext } from '../Providers/UserProvider';
+import React from "react";
+import { ZoomMtg } from "@zoom/meetingsdk";
+import { UserContext } from "../Providers/UserProvider";
+import { useNavigate } from "react-router-dom";
 
 // ZoomMtg.preLoadWasm();
 // ZoomMtg.prepareWebSDK();
 
 function ZoomMeetingPage() {
-  const authEndpoint = 'http://localhost:9000/zoom-signature';
-  const sdkKey = 'Jrss6ZCZQMKAfiycYmQgWA';
-  const meetingNumber = '87153880315';
-  const passWord = 'T6ApzqLSfUaW0lOkvRsV7TrbnB3sns.1';
+  const navigate = useNavigate();
+  const user = useContext(UserContext);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
+  const authEndpoint = "http://localhost:9000/zoom-signature";
+  const sdkKey = "Jrss6ZCZQMKAfiycYmQgWA";
+  const meetingNumber = "87153880315";
+  const passWord = "T6ApzqLSfUaW0lOkvRsV7TrbnB3sns.1";
   const userName = `${user.displayName}`;
   const userEmail = `${user.email}`;
-  const leaveUrl = 'http://localhost:5173/userDashboard';
+  const leaveUrl = "http://localhost:5173/userDashboard";
 
   const getSignature = (e) => {
     e.preventDefault();
 
-
     const meetingData = {
       meetingNumber: meetingNumber,
-      role: 0
+      role: 0,
     };
 
     fetch(authEndpoint, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(meetingData)
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(meetingData),
     })
-    .then(response => response.json())
-    .then(data => {
-      startMeeting(data.signature);
-    })
-    .catch(error => {
-      console.error('Error fetching signature:', error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        startMeeting(data.signature);
+      })
+      .catch((error) => {
+        console.error("Error fetching signature:", error);
+      });
   };
 
   const startMeeting = (signature) => {
@@ -49,16 +57,16 @@ function ZoomMeetingPage() {
           userEmail,
           passWord,
           success: (success) => {
-            console.log('Join meeting success', success);
+            console.log("Join meeting success", success);
           },
           error: (error) => {
-            console.error('Join meeting error', error);
-          }
+            console.error("Join meeting error", error);
+          },
         });
       },
       error: (error) => {
-        console.error('Init meeting error', error);
-      }
+        console.error("Init meeting error", error);
+      },
     });
   };
 
